be94bf7a1e1f1d91b2b7f743f49aead856881285
shiroe <fenrirnobaku1058@gmail.com>
2020-05-21 11:28:51 +0700

    commit file sql

---
+++ Documents/script_QLKS.sql
@@ -0,0 +1,213 @@
+ 
+create database [Nhom14_QLKhachSan_version_1]
+GO
+USE [Nhom14_QLKhachSan_version_1]
+GO
+/****** Object:  Table [dbo].[CTHoaDon]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CTHoaDon](
+	[maCTHD] [int] IDENTITY(1,1) NOT NULL,
+	[ngayTraPhong] [date] NULL,
+	[maPhong] [int] NULL,
+	[maHD] [int] NULL,
+	[maDatPhong] [int] NULL,
+	[maDV] [int] NULL,
+	[soLuong] [int] NULL
+ CONSTRAINT [PK_CTHoaDon] PRIMARY KEY CLUSTERED 
+(
+	[maCTHD] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[DatPhong]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DatPhong](
+	[maDatPhong] [int] IDENTITY(1,1) NOT NULL,
+	[maNhanVien] [int] NULL,
+	[maKhachHang] [int] NULL,
+	[maPhong] [int] NULL,
+	[ngayDatPhong] [date] NULL,
+	[ngayDuKienTra] [date] NULL,
+ CONSTRAINT [PK_DatPhong] PRIMARY KEY CLUSTERED 
+(
+	[maDatPhong] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[DichVu]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DichVu](
+	[maDichVu] [int] IDENTITY(1,1) NOT NULL,
+	[tenDichVu] [nvarchar](50) NULL,
+	[giaDichVu] [money] NULL,
+ CONSTRAINT [PK_DichVu] PRIMARY KEY CLUSTERED 
+(
+	[maDichVu] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[HoaDon]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[HoaDon](
+	[maHD] [int] IDENTITY(1,1) NOT NULL,
+	[maNhanVien] [int] NULL,
+	[ngayLapHD] [date] NULL,
+	[tongTien] [money] NULL
+ CONSTRAINT [PK_HoaDon] PRIMARY KEY CLUSTERED 
+(
+	[maHD] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[KhachHang]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[KhachHang](
+	[maKhachHang] [int] IDENTITY(1,1) NOT NULL,
+	[tenKhachHang] [nvarchar](50) NULL,
+	[CMND] [int] NULL,
+	[gioiTinh] [nvarchar](10) NULL,
+	[SDT] [int] NULL,
+ CONSTRAINT [PK_KhachHang] PRIMARY KEY CLUSTERED 
+(
+	[maKhachHang] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[NhanVien]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[NhanVien](
+	[maNhanVien] [int] IDENTITY(1,1) NOT NULL,
+	[hoTen] [nvarchar](50) NULL,
+	[namSinh] [date] NULL,
+	[gioiTinh] [nvarchar](10) NULL,
+	[diaChi] [nvarchar](150) NULL,
+	[SDT] [int] NULL,
+	[trangThai][nvarchar](50) NULL
+ CONSTRAINT [PK_NhanVien] PRIMARY KEY CLUSTERED 
+(
+	[maNhanVien] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Phong]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Phong](
+	[maPhong] [int] IDENTITY(1,1) NOT NULL,
+	[tenPhong] [nvarchar](50) NULL,
+	[trangThai] [int] NULL,
+	[ghiChu] [nvarchar](150) NULL,
+	[giaPhong] [money] NULL,
+	[loaiPhong] [nvarchar](50) NULL,
+	[maDichVu] [int] NULL,
+ CONSTRAINT [PK_Phong] PRIMARY KEY CLUSTERED 
+(
+	[maPhong] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[TaiKhoan]    Script Date: 5/19/2020 3:06:32 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TaiKhoan](
+	[maTaiKhoan] [int] IDENTITY(1,1) NOT NULL,
+	[userName] [nvarchar](50) NOT NULL,
+	[passWord] [nvarchar](50) NOT NULL,
+	[maNhanVien] [int] NULL,
+	[maNQL] [int] NULL,
+	[loaiTaiKhoan] [nvarchar](20) NOT NULL,
+ CONSTRAINT [PK_TaiKhoan] PRIMARY KEY CLUSTERED 
+(
+	[maTaiKhoan] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+CREATE TABLE [dbo].[NguoiQuanLy](
+	[maNQL] [int] IDENTITY(1,1) NOT NULL,
+	[tenNQL] [nvarchar](50) NOT NULL,
+	
+ CONSTRAINT [PK_NguoiQuanLy] PRIMARY KEY CLUSTERED 
+(
+	[maNQL] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[CTHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_CTHoaDon_DatPhong] FOREIGN KEY([maDatPhong])
+REFERENCES [dbo].[DatPhong] ([maDatPhong])
+GO
+ALTER TABLE [dbo].[CTHoaDon] CHECK CONSTRAINT [FK_CTHoaDon_DatPhong]
+GO
+ALTER TABLE [dbo].[CTHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_CTHoaDon_HoaDon] FOREIGN KEY([maHD])
+REFERENCES [dbo].[HoaDon] ([maHD])
+GO
+ALTER TABLE [dbo].[CTHoaDon] CHECK CONSTRAINT [FK_CTHoaDon_HoaDon]
+GO
+ALTER TABLE [dbo].[CTHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_CTHoaDon_Phong] FOREIGN KEY([maPhong])
+REFERENCES [dbo].[Phong] ([maPhong])
+GO
+ALTER TABLE [dbo].[CTHoaDon] CHECK CONSTRAINT [FK_CTHoaDon_Phong]
+GO
+ALTER TABLE [dbo].[DatPhong]  WITH CHECK ADD  CONSTRAINT [FK_DatPhong_KhachHang] FOREIGN KEY([maDatPhong])
+REFERENCES [dbo].[KhachHang] ([maKhachHang])
+GO
+ALTER TABLE [dbo].[DatPhong] CHECK CONSTRAINT [FK_DatPhong_KhachHang]
+GO
+ALTER TABLE [dbo].[DatPhong]  WITH CHECK ADD  CONSTRAINT [FK_DatPhong_NhanVien] FOREIGN KEY([maNhanVien])
+REFERENCES [dbo].[NhanVien] ([maNhanVien])
+GO
+ALTER TABLE [dbo].[DatPhong] CHECK CONSTRAINT [FK_DatPhong_NhanVien]
+GO
+ALTER TABLE [dbo].[HoaDon]  WITH CHECK ADD  CONSTRAINT [FK_HoaDon_NhanVien] FOREIGN KEY([maNhanVien])
+REFERENCES [dbo].[NhanVien] ([maNhanVien])
+GO
+ALTER TABLE [dbo].[HoaDon] CHECK CONSTRAINT [FK_HoaDon_NhanVien]
+GO
+ALTER TABLE [dbo].[Phong] CHECK CONSTRAINT [FK_Phong_DichVu]
+GO
+ALTER TABLE [dbo].[TaiKhoan]  WITH CHECK ADD  CONSTRAINT [FK_TaiKhoan_NhanVien] FOREIGN KEY([maNhanVien])
+REFERENCES [dbo].[NhanVien] ([maNhanVien])
+GO
+ALTER TABLE [dbo].[TaiKhoan] CHECK CONSTRAINT [FK_TaiKhoan_NhanVien]
+GO
+ALTER TABLE [dbo].[TaiKhoan]  WITH CHECK ADD  CONSTRAINT [FK_TaiKhoan_NguoiQuanLy] FOREIGN KEY([maNQL])
+REFERENCES [dbo].[NguoiQuanLy] ([maNQL])
+GO
+ALTER TABLE [dbo].[TaiKhoan] CHECK CONSTRAINT [FK_TaiKhoan_NhanVien]
+GO
+ALTER TABLE [dbo].[CTHoaDon]  WITH CHECK ADD  CONSTRAINT [FK_CTHoaDon_DichVu] FOREIGN KEY([maDV])
+REFERENCES [dbo].[DichVu] ([maDichVu])
+GO
+ALTER TABLE [dbo].[CTHoaDon] CHECK CONSTRAINT [FK_CTHoaDon_DichVu]
+GO	
+--DROP DATABASE [Nhom14_QLKhachSan];
